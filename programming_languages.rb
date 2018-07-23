46514f07a81caac266b4ac767e1fbe0133a50586
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-23 19:10:40 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,20 @@
+require 'pry'
+
 def reformat_languages(languages)
-  new_hash = {}
-  p first_level = languages[]
+  types= []
+  new_hash= {}
+  
+  languages.each do |type, hash|
+    hash.each do |lang, mohash|
+        new_hash[lang] = mohash
+        new_hash[lang][:style] = []
+    end
+  end
+  languages.each do |type, hash|
+    hash.each do |lang, mohash|
+      binding.pry
+      new_hash[lang][:style] << type
+    end
+  end
+  new_hash
 end
