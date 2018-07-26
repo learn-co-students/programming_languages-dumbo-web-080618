d4077bb992023add2d9e5063ff9bd4c459b3b5f6
Brennan Basinger <bdbasinger777@gmail.com>
2018-07-26 11:29:54 -0500

    Finished

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,34 @@
+
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages[:oo].each do |language, description|
+    # language    == :ruby
+    # description == :type => "interpreted"
+    description.each do |type, kind|
+      # type == :type
+      # kind == "interpreted"
+      new_hash[language] = { type => kind, :style => [:oo] }
+    end
+  end
+  languages[:functional].each do |language, description|
+    # language    == :clojure
+    # description == :type => "compiled"
+    description.each do |type, kind|
+      # type == :type
+      # kind == "compiled"
+      if language == :javascript
+        new_hash[language] = { type => kind, :style => [:oo, :functional] }
+      else
+        new_hash[language] = { type => kind, :style => [:functional] }
+      end
+    end
+  end
+  new_hash
 end
+
+
+
+
+
