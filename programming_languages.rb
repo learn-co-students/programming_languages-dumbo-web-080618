05b35bfbe6daabd4aade02bb123700f90a3970bf
Haley Someone <hbarlar1@gmail.com>
2018-08-01 23:34:13 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |styles, hashes|
+    hashes.each do |language, language_data|
+      language_data.each do |type, type_value|
+        if new_hash.include?(language)
+          new_hash[language][:style] << styles
+        else
+          new_hash[language] = {:type => type_value, :style => [styles]}
+        end
+      end
+    end
+  end
+  new_hash
 end
